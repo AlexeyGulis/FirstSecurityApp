3654ccddc93df3d80d5f95336f4b75e66b8a2ad7
AlexeyGulis <kiri.dh@yandex.ru>
2022-08-04 11:23:48 +0300

    Delete custom implementation

--- src/main/java/by/gulis/springcourse/FirstSecurityApp/config/SecurityConfiguration.java
+++ src/main/java/by/gulis/springcourse/FirstSecurityApp/config/SecurityConfiguration.java
@@ -1,22 +1,29 @@
 package by.gulis.springcourse.FirstSecurityApp.config;
 
-import by.gulis.springcourse.FirstSecurityApp.security.AuthProviderImp;
+import by.gulis.springcourse.FirstSecurityApp.services.PersonDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-    private final AuthProviderImp authProviderImp;
+    private final PersonDetailsService personDetailsService;
 
     @Autowired
-    public SecurityConfiguration(AuthProviderImp authProviderImp) {
-        this.authProviderImp = authProviderImp;
+    public SecurityConfiguration(PersonDetailsService personDetailsService) {
+        this.personDetailsService = personDetailsService;
     }
 
-    protected void configure(AuthenticationManagerBuilder auth) {
-        auth.authenticationProvider(authProviderImp);
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(personDetailsService);
+    }
+    @Bean
+    public PasswordEncoder getPasswordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
     }
 }
